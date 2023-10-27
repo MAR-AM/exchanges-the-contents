b8457cd95698bde57812a9c71b2809a74a58d5b0
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-10-27 23:23:03 +0100

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧

---
+++ exchanges the contents.py
@@ -0,0 +1,5 @@
+A=float(input('Enter the first number : '))
+B=float(input('Enter the second number : '))
+print('The value of the first number will be :',B)
+print('the value of the second number will be :',A)
+
